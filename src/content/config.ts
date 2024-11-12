6d3da9537200e4029d3c9e1b6140f601642f26da
Kyren223 <Kyren223@proton.me>
2024-11-12 22:33:11 +0200

    Backup

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,5 +3,10 @@ import { defineCollection, z } from "astro:content";
 export const collections = {
   blogs: defineCollection({
     type: "content",
+    schema: z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      date: z.date(),
+    }),
   }),
 };
